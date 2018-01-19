166e02f5a62899b8d09f8e4f397aeb5cb7cecd0f
Mahesh Makani <mahesh.makani@netnatives.co.uk>
2018-01-19 14:30:01 +0000

    fix tests

--- src/api/game/index.test.js
+++ src/api/game/index.test.js
@@ -20,7 +20,7 @@ beforeEach(async () => {
 test('POST /games 201 (admin)', async () => {
   const { status, body } = await request(app())
     .post(`${apiRoot}`)
-    .send({ access_token: adminSession, name: 'test', short: 'test', imageUrl: 'test', gbUrl: 'test', meta: 'test' })
+    .send({ access_token: adminSession, name: 'test', short: 'test', imageUrl: 'test', bgUrl: 'test', meta: 'test' })
   expect(status).toBe(201)
   expect(typeof body).toEqual('object')
   expect(body.name).toEqual('test')
@@ -74,7 +74,7 @@ test('GET /games/:id/tournaments 200', async () => {
 test('PUT /games/:id 200 (admin)', async () => {
   const { status, body } = await request(app())
     .put(`${apiRoot}/${game.id}`)
-    .send({ access_token: adminSession, name: 'test', short: 'test', imageUrl: 'test', gbUrl: 'test', meta: 'test' })
+    .send({ access_token: adminSession, name: 'test', short: 'test', imageUrl: 'test', bgUrl: 'test', meta: 'test' })
   expect(status).toBe(200)
   expect(typeof body).toEqual('object')
   expect(body.id).toEqual(game.id)
@@ -101,7 +101,7 @@ test('PUT /games/:id 401', async () => {
 test('PUT /games/:id 404 (admin)', async () => {
   const { status } = await request(app())
     .put(apiRoot + '/123456789098765432123456')
-    .send({ access_token: adminSession, name: 'test', short: 'test', imageUrl: 'test', gbUrl: 'test', meta: 'test' })
+    .send({ access_token: adminSession, name: 'test', short: 'test', imageUrl: 'test', bgUrl: 'test', meta: 'test' })
   expect(status).toBe(404)
 })
 
