8053deca932da6dbe693e117153334453f3c6790
Mahesh Makani <mahesh.makani@netnatives.co.uk>
2018-04-20 20:43:37 +0100

    add game methods + tests

---
+++ src/api/v1/game/controller.js
@@ -0,0 +1,51 @@
+import { Types } from 'mongoose'
+
+import { success, notFound, badImplementation } from '../../../services/response/'
+import Game from '.'
+import Tournament from '../tournament'
+
+const ObjectId = Types.ObjectId
+
+export const create = ({ bodymen: { body } }, res, next) =>
+  Game.create(body)
+    .then((game) => game.view(true))
+    .then(success(res, 201))
+    .catch(badImplementation)
+
+export const index = (req, res, next) =>
+  Game.find({})
+    .then((games) => games.map((game) => game.view()))
+    .then(success(res))
+    .catch(badImplementation)
+
+export const show = ({ params }, res, next) =>
+  Game.findById(params.id)
+    .then(notFound(res))
+    .then((game) => game ? game.view() : null)
+    .then(success(res))
+    .catch(badImplementation)
+
+export const update = ({ bodymen: { body }, params }, res, next) =>
+  Game.findById(params.id)
+    .then(notFound(res))
+    .then((game) => game ? Object.assign(game, body).save() : null)
+    .then((game) => game ? game.view(true) : null)
+    .then(success(res))
+    .catch(badImplementation)
+
+export const destroy = ({ params }, res, next) =>
+  Game.findById(params.id)
+    .then(notFound(res))
+    .then((game) => game ? game.remove() : null)
+    .then(success(res, 204))
+    .catch(badImplementation)
+
+export const tournaments = ({ params }, res, next) =>
+  Tournament.find({ _gameId: ObjectId(params.id) })
+    .populate({
+      path: '_gameId',
+      select: 'name id'
+    })
+    .then(tournaments => tournaments.map(tournament => tournament.view()))
+    .then(success(res))
+    .catch(next)
---
+++ src/api/v1/game/index.test.js
@@ -0,0 +1,133 @@
+
+import request from 'supertest'
+import { apiRoot } from '../../../config'
+import express from '../../../services/express'
+import Game, { GameRouter } from '.'
+import Tournament from '../tournament'
+
+const app = () => express(apiRoot, GameRouter)
+
+let game
+
+beforeEach(async () => {
+  game = await Game.create({})
+  await Tournament.create({
+    _gameId: game._id
+  })
+})
+
+test('POST /games 201 (admin)', async () => {
+  const { status, body } = await request(app())
+    .post(`${apiRoot}?access_token=admin`)
+    .send({ name: 'test', short: 'test', imageUrl: 'test', bgUrl: 'test', meta: 'test' })
+  expect(status).toBe(201)
+  expect(typeof body).toEqual('object')
+  expect(body.name).toEqual('test')
+  expect(body.short).toEqual('test')
+  expect(body.imageUrl).toEqual('test')
+  expect(body.bgUrl).toEqual('test')
+  expect(body.meta).toEqual('test')
+})
+
+test('POST /games 403 (user)', async () => {
+  const { status } = await request(app())
+    .post(`${apiRoot}?access_token=user`)
+    .send({})
+  expect(status).toBe(403)
+})
+
+test('POST /games 401', async () => {
+  const { status } = await request(app())
+    .post(`${apiRoot}`)
+  expect(status).toBe(401)
+})
+
+test('GET /games 200', async () => {
+  const { status, body } = await request(app())
+    .get(`${apiRoot}`)
+  expect(status).toBe(200)
+  expect(Array.isArray(body)).toBe(true)
+})
+
+test('GET /games/:id 200', async () => {
+  const { status, body } = await request(app())
+    .get(`${apiRoot}/${game.id}`)
+  expect(status).toBe(200)
+  expect(typeof body).toEqual('object')
+  expect(body.id).toEqual(game.id)
+})
+
+test('GET /games/:id 404', async () => {
+  const { status } = await request(app())
+    .get(apiRoot + '/123456789098765432123456')
+  expect(status).toBe(404)
+})
+
+test('PUT /games/:id 200 (admin)', async () => {
+  const { status, body } = await request(app())
+    .put(`${apiRoot}/${game.id}?access_token=admin`)
+    .send({ name: 'test', short: 'test', imageUrl: 'test', bgUrl: 'test', meta: 'test' })
+  expect(status).toBe(200)
+  expect(typeof body).toEqual('object')
+  expect(body.id).toEqual(game.id)
+  expect(body.name).toEqual('test')
+  expect(body.short).toEqual('test')
+  expect(body.imageUrl).toEqual('test')
+  expect(body.bgUrl).toEqual('test')
+  expect(body.meta).toEqual('test')
+})
+
+test('PUT /games/:id 403 (user)', async () => {
+  const { status } = await request(app())
+    .put(`${apiRoot}/${game.id}?access_token=user`)
+    .send({})
+  expect(status).toBe(403)
+})
+
+test('PUT /games/:id 401', async () => {
+  const { status } = await request(app())
+    .put(`${apiRoot}/${game.id}`)
+  expect(status).toBe(401)
+})
+
+test('PUT /games/:id 404 (admin)', async () => {
+  const { status } = await request(app())
+    .put(apiRoot + '/123456789098765432123456?access_token=admin')
+    .send({ name: 'test', short: 'test', imageUrl: 'test', bgUrl: 'test', meta: 'test' })
+  expect(status).toBe(404)
+})
+
+test('DELETE /games/:id 204 (admin)', async () => {
+  const { status } = await request(app())
+    .delete(`${apiRoot}/${game.id}?access_token=admin`)
+    .query({})
+  expect(status).toBe(204)
+})
+
+test('DELETE /games/:id 403 (user)', async () => {
+  const { status } = await request(app())
+    .delete(`${apiRoot}/${game.id}?access_token=user`)
+    .query({})
+  expect(status).toBe(403)
+})
+
+test('DELETE /games/:id 401', async () => {
+  const { status } = await request(app())
+    .delete(`${apiRoot}/${game.id}`)
+  expect(status).toBe(401)
+})
+
+test('DELETE /games/:id 404 (admin)', async () => {
+  const { status } = await request(app())
+    .delete(apiRoot + '/123456789098765432123456?access_token=admin')
+    .query({})
+  expect(status).toBe(404)
+})
+
+test('GET /games/:id/tournaments 200', async () => {
+  const { status, body } = await request(app())
+    .get(`${apiRoot}/${game.id}/tournaments`)
+  expect(status).toBe(200)
+  expect(Array.isArray(body)).toBe(true)
+  expect(body.length).toEqual(1)
+})
