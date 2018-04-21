59d2fedf7a2ada5a1a408ee166081674fb356492
Mahesh Makani <mahesh.makani@netnatives.co.uk>
2018-04-21 14:03:22 +0100

    add result routes and tests

--- src/api/v1/result/index.js
+++ src/api/v1/result/index.js
@@ -1,7 +1,21 @@
 import { Router } from 'express'
-import Result from './model'
+import { middleware as body } from 'bodymen'
+import Result, { schema } from './model'
+import { isAdmin } from '../../../services/auth'
+import { create, index, show, update, destroy } from './controller'
+const { _playerId, _tournamentId, rank, meta } = schema.tree
 
 const ResultRouter = new Router()
+
+ResultRouter.post('/', isAdmin, body({ _playerId, _tournamentId, rank, meta }), create)
+
+ResultRouter.get('/', index)
+
+ResultRouter.get('/:id', show)
+
+ResultRouter.put('/:id', isAdmin, body({ _playerId, _tournamentId, rank, meta }), update)
+
+ResultRouter.delete('/:id', isAdmin, destroy)
 
 export default Result
 export { ResultRouter }
