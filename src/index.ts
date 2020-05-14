ad9709c8263ab3b690558ff4c485f7df8e6634e7
Mahesh Makani <maheshmakani@gmail.com>
2020-05-14 11:01:33 +0100

    set up typescript, mongoose, docker mongo

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log('hello world');
+import { default as mongoose, Document, Schema } from 'mongoose';
+
+mongoose.connect(process.env.MONGODB_URI || '', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+interface Cat extends Document {
+  name: string;
+}
+
+const CatSchema: Schema = new Schema({
+  name: String,
+});
+
+const Cat = mongoose.model<Cat>('Cat', CatSchema);
+
+Cat.find({}).then((cats) => {
+  if (!cats.length) {
+    new Cat({ name: 'Morgana' }).save().then((cat) => {
+      console.log(cat.name);
+    });
+  } else {
+    console.log(cats);
+  }
+});
+
+console.log('hello cats');
