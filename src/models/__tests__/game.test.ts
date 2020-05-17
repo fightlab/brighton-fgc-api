76d69384e070898fc79929aae356cb261c77e6af
Mahesh Makani <maheshmakani@gmail.com>
2020-05-17 21:50:42 +0100

    changing game test, add debug port

--- src/models/__tests__/game.test.ts
+++ src/models/__tests__/game.test.ts
@@ -1,24 +1,43 @@
 import { Game, IGame } from '@models/game';
-import { dataLengthsDefault } from '@lib/faker';
+import { default as faker } from 'faker';
 
 describe('Game model test', () => {
+  const game: IGame = {
+    name: 'Blade Arcus from Shining',
+    short: 'BAFS',
+    bg: faker.image.imageUrl(),
+    logo: faker.image.imageUrl(),
+    meta: {
+      random: 'stuff',
+    },
+  };
+
   const gameMin: IGame = {
     name: 'Persona 5 Arena',
     short: 'P5A',
   };
 
   it('create & save game successfully', async () => {
+    const input = new Game(game);
+    const output = await input.save();
+
+    expect(output._id).toBeDefined();
+    expect(output.name).toBe(game.name);
+    expect(output.short).toBe(game.short);
+    expect(output.bg).toBe(game.bg);
+    expect(output.logo).toBe(game.logo);
+    expect(output.meta).toBeDefined();
+  });
+
+  it('create & save minimum game successfully', async () => {
     const input = new Game(gameMin);
     const output = await input.save();
 
     expect(output._id).toBeDefined();
     expect(output.name).toBe(gameMin.name);
     expect(output.short).toBe(gameMin.short);
-  });
-
-  it('should count total number of games', async () => {
-    const output = (await Game.find()).length;
-
-    expect(output).toBe(dataLengthsDefault.game + 1);
+    expect(output.bg).toBeUndefined();
+    expect(output.logo).toBeUndefined();
+    expect(output.meta).toBeUndefined();
   });
 });
